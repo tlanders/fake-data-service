4d5a294ce4c54f113137f2f532216d0c4ab22561
tlanders <tlanders@landersconsulting.biz>
2026-01-02 11:33:23 -0600

    initial commit

---
+++ src/main/java/biz/lci/demo/fakedataservice/FakeDataServiceApplication.java
@@ -0,0 +1,13 @@
+package biz.lci.demo.fakedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/biz/lci/demo/fakedataservice/FakeDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.lci.demo.fakedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
